8902a0710430e837856c46d455df010dc67f8c46
Brian Woodward <brian.woodward@gmail.com>
2014-11-06 23:03:10 -0500

    setup

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * engine-assemble <https://github.com/doowb/engine-assemble>
+ *
+ * Copyright (c) 2014 Brian Woodward, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
